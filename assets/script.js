435e51707a8324f2c921cda1ab7135c6636c1f7f
John H <50893052+John-Hoy@users.noreply.github.com>
2023-01-11 10:57:01 -0700

    basic js layout added
    set name of site to Sudoku haven, created simple start button, setup js functions for various elements

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-// Main JS script
+
+//? creates board based on user selected input of difficulty
+function generateBoard() {
+
+}
+
+//? increases the tracker for number of games played by the user
+function gameCount() {
+
+}
+
+
+//? when the start game button is pressed it should, generate a new board, and increase number of games played by one
+function startGame() {
+    generateBoard();
+    gameCount();
+    console.log("Game started!");
+}
+
+
+const startGameButton = document.getElementById("start-game");
+startGameButton.addEventListener("click", startGame);// Main JS script
