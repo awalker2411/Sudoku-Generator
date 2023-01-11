d19f81ffbb03171b6ce403eb8f5a7b911c730548
John H <50893052+John-Hoy@users.noreply.github.com>
2023-01-11 13:23:17 -0700

    added 16x16 board
    api for 16x16 board and removed all merged issues with last push

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,24 @@
+//? board array, will fill as it gets the set numbers from our generateBoard
+const board = Array(9).fill(null).map(() => Array(9).fill(null));
 
-
-    //? board array, will fill as it gets the set numbers from our generateBoard
-    const board = Array(9).fill(null).map(() => Array(9).fill(null));
-
-
-    //! Api for Solving the code
-    const solver = {
-        method: 'POST',
+function generateBoard16() {
+    //! Api for 16x16 + its key
+    const options = {
+        method: 'GET',
         headers: {
-            'content-type': 'application/json',
             'X-RapidAPI-Key': '72d127291bmsh1e75b0ade2c691bp11f226jsn75f66a8bbb06',
-            'X-RapidAPI-Host': 'sudoku-solver3.p.rapidapi.com'
-        },
-
-        body: '{"input":[number array here use 0 for missing numbers]}'
+            'X-RapidAPI-Host': 'mega-sudoku-generator.p.rapidapi.com'
+        }
     };
 
-    fetch('https://sudoku-solver3.p.rapidapi.com/sudokusolver/', solver)
+    fetch('https://mega-sudoku-generator.p.rapidapi.com/mega', options)
         .then(response => response.json())
-        .then(response => console.log(response.puzzle))
+        .then(response => console.log(response))
         .catch(err => console.error(err));
-    //! This is our API for generating the sudoku
+}
+//? creates board based on user selected input of difficulty
+function generateBoard9() {
+    //! This is our API for generating the sudoku 9x9 + its key
     const creator = {
         method: 'GET',
         headers: {
@@ -33,11 +31,6 @@
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
-
-    //? creates board based on user selected input of difficulty
-    function generateBoard() {
-
-    }
 
     //? increases the tracker for number of games played by the user
     function gameCount() {
@@ -55,32 +48,3 @@
 
     const startGameButton = document.getElementById("start-game");
     startGameButton.addEventListener("click", startGame);// Main JS script
-    
-//? when the start game button is pressed it should generate a new board
-function startGame() {
-    generateBoard();
-    console.log("Game started!");
-}
-
-// When the hint button is pressed, should color code user inputs with green if correct and red if incorrect
-function giveHint() {
-
-}
-
-// When the solve button is pressed, wipe user inputs and generate correct numbers for the sudoku board and tell the user how many incorrect entries that they had
-// Should also update the 'Completed Puzzles' number and 'Attempted Puzzles' number
-function solveGame() {
-    gameCount();
-}
-
-// Listener for start button being clicked
-const startGameButton = document.getElementById("start-game");
-startGameButton.addEventListener("click", startGame);// Main JS script
-
-// Listener for hint button being clicked
-const hintGameButton = document.getElementById("hint-game");
-startGameButton.addEventListener("click", giveHint);
-
-//Listener for solve button being clicked
-const solveGameButton = document.getElementById("solve-game");
-startGameButton.addEventListener("click", solveGame);
