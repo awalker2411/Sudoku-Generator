d5e98e9645856b36101a448002b78a47ebc01a65
John H <50893052+John-Hoy@users.noreply.github.com>
2023-01-12 11:30:45 -0700

    set responses to be saved locally
    response set ot be put into local memory to be pulled into other functions from the getitem

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,14 @@ const hintGameButton = document.getElementById("hint-game");
 const solveGameButton = document.getElementById("solve-game");
 const gameTracker = document.getElementById("#games")
 
-let puzzle = "";
+let puzzle9 = "";
+let solution9 = "";
 const puzzleArray = [];
-let key = "";
 const keyArray = [];
+localStorage.getItem('puzzleArray', puzzle9);
+localStorage.getItem('solution', solution9);
+console.log("outisde function", solution9, puzzle9)
+
 
 
 //? board array, will fill as it gets the set numbers from our generateBoard
@@ -26,16 +30,19 @@ function generateBoard16() {
 
     fetch('https://mega-sudoku-generator.p.rapidapi.com/mega', options)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => makeBoard(response))
         .catch(err => console.error(err));
 
-
-    for (let r = 0; r < 16; r++) {
-        for (let c = 0; c < 16; c++) {
-            let tile = document.createElement("div");
-            tile.id = // will be the value of the cell
-                tile.classList.add("tile");
-            document.getElementById("board").append(tile);
+    function makeBoard(response) {
+        let puzzle16 = response.puzzle;
+        console.log(puzzle16)
+        for (let r = 0; r < 16; r++) {
+            for (let c = 0; c < 16; c++) {
+                let tile = document.createElement("div");
+                tile.id = // will be the value of the cell
+                    tile.classList.add("tile");
+                document.getElementById("board").append(tile);
+            }
         }
     }
 }
@@ -54,8 +61,6 @@ function startGame() {
     fetch('https://sudoku-service.p.rapidapi.com/v1/sudoku?withSolution=true', creator)
         .then(response => response.json())
         .then(response => makeBoard(response))
-        //* json.data.puzzle
-        //*json.data.solution
         .catch(err => console.error(err));
 
     //? increases the tracker for number of games played by the user
@@ -63,18 +68,23 @@ function startGame() {
         gameTracker.textContent++;
     }
     //? when the start game button is pressed it should generate a new board
-function makeBoard(response){
-    let puzzle = response.puzzle;
-    console.log(puzzle)
-    for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div");
-            tile.id = // will be the value of the cell
-                tile.classList.add("tile");
-            document.getElementById("board").append(tile);
+    function makeBoard(response) {
+        let puzzle9 = response.puzzle;
+        let puzzleArray = Array.from(response.puzzle);
+        let solution = response.solution;
+        localStorage.setItem('puzzleArray', puzzleArray);
+        localStorage.setItem('solution', solution);
+        console.log("inside function", solution9, puzzle9)
+
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                let tile = document.createElement("div");
+                tile.id = // will be the value of the cell
+                    tile.classList.add("tile");
+                document.getElementById("board").append(tile);
+            }
         }
     }
-}
 }
 
 // When the hint button is pressed, should color code user inputs with green if correct and red if incorrect
@@ -82,26 +92,26 @@ function giveHint() {
 
 
 
-// When the solve button is pressed, wipe user inputs and generate correct numbers for the sudoku board and tell the user how many incorrect entries that they had
-// Should also update the 'Completed Puzzles' number and 'Attempted Puzzles' number
-function solveGame() {
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '72d127291bmsh1e75b0ade2c691bp11f226jsn75f66a8bbb06',
-            'X-RapidAPI-Host': 'sudoku-service.p.rapidapi.com'
-        },
-        //? needs to be setup to take our puzzle board  
-        body: '{"board":[[5,0,0,0,9,0,0,0,0],[7,9,0,0,0,0,0,0,8],[0,6,0,8,0,0,0,2,5],[9,0,6,0,0,5,0,0,0],[0,0,0,0,0,0,0,4,0],[0,0,0,0,7,1,0,0,0],[0,7,3,0,0,0,4,5,0],[0,0,4,0,6,0,2,0,0],[0,0,0,2,0,0,8,0,0]]}'
-    };
-    //?this will generate the solution 
-    fetch('https://sudoku-service.p.rapidapi.com/v1/sudoku/solve?count=2', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    // When the solve button is pressed, wipe user inputs and generate correct numbers for the sudoku board and tell the user how many incorrect entries that they had
+    // Should also update the 'Completed Puzzles' number and 'Attempted Puzzles' number
+    function solveGame() {
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '72d127291bmsh1e75b0ade2c691bp11f226jsn75f66a8bbb06',
+                'X-RapidAPI-Host': 'sudoku-service.p.rapidapi.com'
+            },
+            //? needs to be setup to take our puzzle board  
+            body: '{"board":[[5,0,0,0,9,0,0,0,0],[7,9,0,0,0,0,0,0,8],[0,6,0,8,0,0,0,2,5],[9,0,6,0,0,5,0,0,0],[0,0,0,0,0,0,0,4,0],[0,0,0,0,7,1,0,0,0],[0,7,3,0,0,0,4,5,0],[0,0,4,0,6,0,2,0,0],[0,0,0,2,0,0,8,0,0]]}'
+        };
+        //?this will generate the solution 
+        fetch('https://sudoku-service.p.rapidapi.com/v1/sudoku/solve?count=2', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    }
 }
-
 // Listener for start button being clicked
 startGameButton.addEventListener("click", startGame);// Main JS script
 
