31684687ad6399dd4baea5bb8daa0a60ffca7698
John H <50893052+John-Hoy@users.noreply.github.com>
2023-01-17 13:34:47 -0700

    buttons now input for board
    when the keyboard on screen is pressed after an cell is selected it will now replace it with the value of the button

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,6 @@ function startGame() {
     attemptedPuzzles++;
 
     //! This is our API for generating the sudoku 9x9 + its key
-    //! This is our API for generating the sudoku 9x9 + its key
     const creator = {
         method: 'GET',
         headers: {
@@ -179,3 +178,25 @@ hintGameButton.addEventListener("click", giveHint);
 
 //Listener for solve button being clicked
 //solveGameButton.addEventListener("click", solveGame);
+//?numberpad functionality
+let selectedSpan = null;
+
+document.addEventListener("click", function (event) {
+    //checks to see if you selected a td or a span
+    if (event.target.matches("td") || event.target.matches("span")) {
+        if (selectedSpan) {
+            selectedSpan.classList.remove('selected');
+        }
+        if (event.target.matches("td")) {
+            selectedSpan = event.target.querySelector("span");
+        } else {
+            selectedSpan = event.target;
+        }
+        selectedSpan.classList.add('selected');
+    }
+    if(event.target.matches("button")){
+        if(selectedSpan !== null){
+            selectedSpan.innerHTML = event.target.dataset.value;
+        }
+    }
+});
